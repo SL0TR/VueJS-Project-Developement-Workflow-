6779cef83b99bb4f4226c336fe749bba42ed1a20
SL0TR <yomaimsj@gmail.com>
2018-01-30 22:51:55 +0600

    Connected firebase api

--- src/App.vue
+++ src/App.vue
@@ -32,7 +32,7 @@
     },
     methods: {
       submit() {
-        this.$http.post('https://vue-js-http-4112c.firebaseio.com/data.json', this.user).
+        this.$http.post('', this.user).
           then(response => {
             console.log(response)
           }, error => {
@@ -41,7 +41,7 @@
 
       },
       fetch() {
-        this.$http.get('https://vue-js-http-4112c.firebaseio.com/data.json')
+        this.$http.get('')
           .then(response => {
             return response.json()
           })
--- src/main.js
+++ src/main.js
@@ -2,7 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import VueResource from 'vue-resource'
 
-Vue.use(VueResource);
+Vue.use(VueResource)
+
+Vue.http.options.root = 'https://vue-js-http-4112c.firebaseio.com/data.json'
+
 
 new Vue({
   el: '#app',
