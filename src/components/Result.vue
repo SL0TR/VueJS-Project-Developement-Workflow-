dae94cfa014250f53a313f2c9b91372d55f0a959
SL0TR <yomaimsj@gmail.com>
2018-02-21 22:47:08 +0600

    added getters for computed method

--- src/components/Result.vue
+++ src/components/Result.vue
@@ -1,13 +1,21 @@
 <template>
-    <p>Counter is: {{ counter }}</p>
+  <div>
+    <p>Counter is: {{ doubleCounter }}</p>
+    <p>Number of Clicks:  {{ clickCounter }}</p>
+  </div>
 </template>
 
 <script>
-    export default {
-        computed: {
-            counter() {
-              return  this.$store.state.counter
-            }
-        }
+import { mapGetters } from 'vuex'
+export default {
+  computed: {
+    ...mapGetters([
+    'clickCounter',
+    'doubleCounter'
+    ]),
+    ourOwn() {
+      
     }
+  }
+}
 </script>
