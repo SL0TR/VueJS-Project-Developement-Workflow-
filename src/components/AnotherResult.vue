c1f3afa294ba3fda55e2057ac46f5c388ee21aa2
SL0TR <yomaimsj@gmail.com>
2018-04-03 17:08:24 +0600

    use mapGetters to use multiple computed methods directly in component

--- src/components/AnotherResult.vue
+++ src/components/AnotherResult.vue
@@ -1,16 +1,19 @@
 <template>
   <div>
-    <p>Another Counter is: {{ counter }} </p>
-    <!-- <p>Number of Clicks: </p> -->
+    <p>Another Counter is: {{ doubleCounter }} </p>
+    <p>Number of Clicks: {{ stringCounter }} </p>
   </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
+
 export default {
-  computed: {
-    counter () {
-      return this.$store.getters.doubleCounter;
-    }
+  computed: { 
+    ...mapGetters([
+      'doubleCounter',
+      'stringCounter'
+    ]) 
   }
 }
 </script>
